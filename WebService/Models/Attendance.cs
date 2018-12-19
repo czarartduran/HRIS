f1d6f8029571a7f20c7fd5fd1cea1f14ef20f077
czarartduran@ymail.com <*Paquitogapi27>
2018-12-19 17:22:11 +0800

    update

---
+++ WebService/Models/Attendance.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebService.Models
+{
+    public class Attendance
+    {
+        public Int64 TimesheetID { get; set; }
+        public DateTime AttendanceDate { get; set; }
+        public DateTime TimeIN { get; set; }
+        public DateTime TimeOUT { get; set; }
+        public DateTime OverTimeIN { get; set; }
+        public DateTime OverTimeOUT { get; set; }
+        public Int32 Late { get; set; }
+        public Int32 UnderTime { get; set; }
+        public Int32 OverTime { get; set; }
+        public Int32 GrandTotal { get; set; }
+    }
+}
--- WebService/WebService.asmx.cs
+++ WebService/WebService.asmx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Services;
 using System.Configuration;
+using System.Diagnostics;
 
 namespace WebService
 {
@@ -16,18 +17,34 @@ namespace WebService
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
     // [System.Web.Script.Services.ScriptService]
     public class WebService : System.Web.Services.WebService
-    {
-
-        [WebMethod]
-        public string HelloWorld()
-        {
-            return "Hello World";
-        }
-
+    {        
         [WebMethod]
         public DateTime WebApplicationDateTime()
         {
             return TimeZoneInfo.ConvertTimeBySystemTimeZoneId(DateTime.Now, ConfigurationManager.AppSettings["TimeZone"]);
+        }
+
+        [WebMethod]
+        public bool RegistereTimeLog(string EmployeeID)
+        {
+            bool ans = false;
+            try
+            {
+                string timezone = ConfigurationManager.AppSettings["TimeZone"];
+                Debug.WriteLine("TimeZone: " + timezone);
+                DateTime sysdate = TimeZoneInfo.ConvertTimeBySystemTimeZoneId(DateTime.Now, timezone);
+                DateTime registereddate = Convert.ToDateTime(sysdate.ToString("yyyy/MM/dd"));
+                using (hrisDataContext ta = new hrisDataContext())
+                {
+                    ta.sp_RegisterTimeLog(Convert.ToInt64(EmployeeID), registereddate, sysdate);
+                    ans = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("RegistereTimeLog: " + ex.Message);
+            }
+            return ans;
         }
     }
 }
